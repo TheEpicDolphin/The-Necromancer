6523a9a5100693a51178bcfec4b413473286d04e
gdolphin <gdolphin@mit.edu>
2020-03-07 02:21:53 -0500

    first commit

---
+++ Assets/Scripts/MeleeAIAlly.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MeleeAIAlly : NavAgent
+{
+    public enum AIState
+    {
+        AttackingEnemy,
+        ReturningToMaster,
+        SearchingForEnemy,
+        Dying,
+        Despawning
+    }
+    AIState state;
+    public Transform master;
+    Transform wanderingTransform;
+
+    //The enemy AI that this ally is targeting
+    MeleeAIEnemy enemyTarget;
+    float nextWanderTime;
+    float WANDER_RADIUS = 30.0f;
+    int health = 150;
+    float nextAttackTime;
+    // Start is called before the first frame update
+    new void Start()
+    {
+        base.Start();
+        nextWanderTime = Time.time;
+        nextAttackTime = Time.time;
+        state = AIState.ReturningToMaster;
+
+        GameObject go = new GameObject();
+        go.transform.position = transform.position;
+        wanderingTransform = go.transform;
+        wanderingTransform.parent = master;
+
+        speed = 15.0f;
+        target = master;
+    }
+
+    public void ExecuteState()
+    {
+        
+        switch (state)
+        {
+            case AIState.AttackingEnemy:
+                if ((master.position - transform.position).magnitude >= WANDER_RADIUS)
+                {
+                    state = AIState.ReturningToMaster;
+                    target = master;
+                    enemyTarget.RemovePursuer(this);
+                    enemyTarget = null;
+                }
+                break;
+            case AIState.ReturningToMaster:
+                if ((master.position - transform.position).magnitude < WANDER_RADIUS)
+                {
+                    state = AIState.SearchingForEnemy;
+                    target = wanderingTransform;
+                }
+                break;
+            case AIState.SearchingForEnemy:
+                if ((master.position - transform.position).magnitude >= WANDER_RADIUS)
+                {
+                    state = AIState.ReturningToMaster;
+                    target = master;
+                }
+                else
+                {
+                    AIManager.Instance.PutOnDispatchQueue(this);
+                    if (Time.time > nextWanderTime)
+                    {
+                        nextWanderTime = Time.time + Random.Range(3.0f, 8.0f);
+                        Vector2 randPos2d = WANDER_RADIUS * Random.insideUnitCircle;
+                        target.position = master.position + new Vector3(randPos2d.x, 0.0f, randPos2d.y);
+                    }
+                }
+                break;
+            case AIState.Dying:
+                //Play dying animation with coroutine maybe
+                state = AIState.Despawning;
+                break;
+            case AIState.Despawning:
+                AIManager.Instance.agents.Remove(this);
+                //Removing from enemies list will prevent this enemy from having its state executed again
+                AIManager.Instance.allies.Remove(this);
+                Destroy(this.gameObject);
+                break;
+        }
+    }
+
+    public void AttackEnemy(MeleeAIEnemy enemyAI)
+    {
+        state = AIState.AttackingEnemy;
+        enemyTarget = enemyAI;
+        target = enemyAI.transform;
+    }
+
+    public void TargetWasKilled()
+    {
+        state = AIState.SearchingForEnemy;
+        enemyTarget = null;
+        target = wanderingTransform;
+    }
+
+    public override void MoveAgent(Vector3 heading)
+    {
+        transform.Translate(heading * Time.deltaTime, Space.World);
+        Vector3 curPos = new Vector3(transform.position.x, 0.1f, transform.position.z);
+        //desiredHeading = TARGET_SPEED * (pathPoints[0] - curPos).normalized;
+        //Smooth movement
+        desiredHeading = Vector3.Lerp(heading, speed * (pathPoints[0] - curPos).normalized, 5.0f * Time.deltaTime);
+        Debug.DrawLine(curPos, curPos + desiredHeading, Color.cyan);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        //Invincible for now
+        //health -= damage;
+        if (health <= 0)
+        {
+            state = AIState.Dying;
+
+            //Disable collider to avoid future triggers
+            gameObject.GetComponent<Collider>().enabled = false;
+
+            enemyTarget.RemovePursuer(this);
+            enemyTarget = null;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (wanderingTransform)
+        {
+            Destroy(wanderingTransform.gameObject);
+        }
+        
+    }
+
+
+    //This gets called before Update functions.
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.GetComponent<MeleeAIEnemy>() && Time.time > nextAttackTime)
+        {
+            //Attack enemy AI
+            MeleeAIEnemy enemyAI = other.gameObject.GetComponent<MeleeAIEnemy>();
+            nextAttackTime = Time.time + Random.Range(0.5f, 1.0f);
+            enemyAI.TakeDamage(20);
+        }
+    }
+}
---
+++ Assets/Scripts/MeleeAIEnemy.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAIEnemy : NavAgent
+{
+    public enum AIState
+    {
+        Attacking,
+        NavigatingToPlayer,
+        Dying,
+        Dead,
+        Despawning
+    }
+    AIState state;
+    public Transform playerTrans;
+    float ATTACK_RADIUS = 30.0f;
+    int health = 100;
+    float nextAttackTime;
+
+    //All the ally AI that are pursuing this enemy
+    List<MeleeAIAlly> pursuers = new List<MeleeAIAlly>();
+
+    //How much more the enemies prefer attacking the player over allies
+    static float PLAYER_PREFERENCE = 2.0f;
+
+    // Start is called before the first frame update
+    new void Start()
+    {
+        base.Start();
+        nextAttackTime = Time.time;
+        speed = 12.0f;
+        state = AIState.NavigatingToPlayer;
+    }
+
+    public void ExecuteState()
+    {
+        //Attack player
+        switch (state)
+        {
+            case AIState.Attacking:
+                //Attack closest of the friendlies and player
+                Transform closestAgent = playerTrans;
+                float minDistance = Vector3.Distance(closestAgent.position, transform.position) / PLAYER_PREFERENCE;
+                foreach(MeleeAIAlly ally in AIManager.Instance.allies)
+                {
+                    float dist = Vector3.Distance(ally.transform.position, transform.position);
+                    if (dist < minDistance)
+                    {
+                        closestAgent = ally.transform;
+                        minDistance = dist;
+                    }
+                }
+                target = closestAgent;
+
+                if ((playerTrans.position - transform.position).magnitude >= ATTACK_RADIUS)
+                {
+                    state = AIState.NavigatingToPlayer;
+                    target = playerTrans;
+                    
+                }
+                //If enemy does not have any pursuers, put it on the queue so that it can be assigned some
+                else if(pursuers.Count == 0)
+                {
+                    AIManager.Instance.PutOnEnemyQueue(this);
+                }
+                break;
+            case AIState.NavigatingToPlayer:
+                if ((playerTrans.position - transform.position).magnitude < ATTACK_RADIUS)
+                {
+                    state = AIState.Attacking;
+                }
+                break;
+            case AIState.Dying:
+                //Play dying animation with coroutine maybe
+                state = AIState.Despawning;
+                break;
+            case AIState.Dead:
+                //In this state, the enemy can potentially be revived by the player. If we wait too long, the enemy
+                //will despawn
+                break;
+            case AIState.Despawning:
+                AIManager.Instance.agents.Remove(this);
+                //Removing from enemies list will prevent this enemy from having its state executed again
+                AIManager.Instance.enemies.Remove(this);
+                Destroy(this.gameObject);
+                break;
+        }
+    }
+
+
+    public override void MoveAgent(Vector3 heading)
+    {
+        transform.Translate(heading * Time.deltaTime, Space.World);
+        Vector3 curPos = new Vector3(transform.position.x, 0.1f, transform.position.z);
+        //desiredHeading = TARGET_SPEED * (pathPoints[0] - curPos).normalized;
+        //Smooth movement
+        desiredHeading = Vector3.Lerp(heading, speed * (pathPoints[0] - curPos).normalized, 5.0f * Time.deltaTime);
+        Debug.DrawLine(curPos, curPos + desiredHeading, Color.cyan);
+
+    }
+
+    public void AddPursuer(MeleeAIAlly ally)
+    {
+        pursuers.Add(ally);
+    }
+
+    public void RemovePursuer(MeleeAIAlly ally)
+    {
+        pursuers.Remove(ally);
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            state = AIState.Dying;
+
+            //Disable collider to avoid future triggers
+            gameObject.GetComponent<Collider>().enabled = false;
+            foreach (MeleeAIAlly friendly in pursuers)
+            {
+                friendly.TargetWasKilled();
+            }
+        }
+    }
+
+    
+    //This gets called before Update functions.
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.GetComponent<MeleeAIAlly>() && Time.time > nextAttackTime)
+        {
+            //Attack ally AI
+            MeleeAIAlly allyAI = other.gameObject.GetComponent<MeleeAIAlly>();
+            nextAttackTime = Time.time + Random.Range(0.5f, 1.0f);
+            allyAI.TakeDamage(20);
+        }
+    }
+}
